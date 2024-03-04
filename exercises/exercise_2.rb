d6350206730380c1abcce5685e3d9bd503775d67
atle <alextle24@gmail.com>
2024-03-04 14:30:11 -0700

    completed exercise 2 and 3

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+# Find the first store and assign it to @store1
+@store1 = Store.find_by(id: 1)
+
+#Find the second store and assign it to @store 2
+@store2 = Store.find_by(id: 2)
+
+@store1.update(name: "Edmonton")
